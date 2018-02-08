ccd6f8a9cfbc0abd28aa8a5f64ceafc8401e0b1e
KenNgo <khoi.ngonguyen@gmail.com>
2018-02-08 13:39:40 +0700

    updates

--- src/scripts/components/App.js
+++ src/scripts/components/App.js
@@ -1,53 +1,52 @@
-// import React, { Component } from 'react';
-// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-// import Home from './common/HomePage';
-// import About from './common/AboutPage';
-
-// class App extends Component {
-//    render() {
-//       return (
-//          <Router>
-//             <div>
-//                <h2>Welcome to React Router Tutorial</h2>
-//                <ul>
-//                   <li><Link to={'/'}>Home</Link></li>
-//                   <li><Link to={'/about'}>About</Link></li>
-//                </ul>
-//                <hr />
-               
-//                <Switch>
-//                   <Route exact path='/' component={Home} />
-//                   <Route exact path='/about' component={About} />
-//                </Switch>
-//             </div>
-//          </Router>
-//       );
-//    }
-// }
-// export default App;
-
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { addTodo } from '../actions/actions'
-
-import AddTodo from './common/AddTodo.js'
-import TodoList from './common/TodoList.js'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './common/HomePage';
+import About from './common/AboutPage';
 
 class App extends Component {
    render() {
-      const { dispatch, visibleTodos } = this.props
-      
       return (
-         <div>
-            <AddTodo onAddClick = {text => dispatch(addTodo(text))} />
-            <TodoList todos = {visibleTodos}/>
-         </div>
-      )
+         <Router>
+            <div>
+               <ul>
+                  <li><Link to={'/'}>Home</Link></li>
+                  <li><Link to={'/about'}>About</Link></li>
+               </ul>
+               <hr />
+               
+               <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+               </Switch>
+            </div>
+         </Router>
+      );
    }
 }
-function select(state) {
-   return {
-      visibleTodos: state.todos
-   }
-}
-export default connect(select)(App);
+export default App;
+
+// import React, { Component } from 'react'
+// import { connect } from 'react-redux'
+// import { addTodo } from '../actions/actions'
+
+// import AddTodo from './common/AddTodo.js'
+// import TodoList from './common/TodoList.js'
+
+// class App extends Component {
+//    render() {
+//       const { dispatch, visibleTodos } = this.props
+      
+//       return (
+//          <div>
+//             <AddTodo onAddClick = {text => dispatch(addTodo(text))} />
+//             <TodoList todos = {visibleTodos}/>
+//          </div>
+//       )
+//    }
+// }
+// function select(state) {
+//    return {
+//       visibleTodos: state.todos
+//    }
+// }
+// export default connect(select)(App);
